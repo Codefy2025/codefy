207426fc5aa48c6ea35a27746137b422d3e648d1
Codefy2025 <niklasjuulrasmussen7@gmail.com>
2026-05-01 20:55:24 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const output = document.getElementById("output");
 
 document.getElementById("load").onclick = async () => {
   try {
-    const res = await fetch("https://your-worker-url.workers.dev/api/content");
+    const res = await fetch("https://sparkling-shape-65b4.niklasjuulrasmussen7.workers.dev/");
     const data = await res.text();
 
     output.textContent = data;
