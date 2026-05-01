f1c5ae80bfff1e62cb148030b5f81291c3a4f127
Codefy2025 <niklasjuulrasmussen7@gmail.com>
2026-05-01 22:44:39 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-const API_URL = "https://sparkling-shape-65b4.niklasjuulrasmussen7.workers.dev/api/home";
-
 async function loadHome() {
-  const res = await fetch(API_URL);
+  const res = await fetch("https://sparkling-shape-65b4.niklasjuulrasmussen7.workers.dev/api/home");
   const data = await res.json();
 
   document.getElementById("title").innerText = data.title;
