4ba0827fa6bd45980d108f70ae3cb24bb892f873
Codefy2025 <niklasjuulrasmussen7@gmail.com>
2026-05-02 17:42:13 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-async function loadHome() {
-  const res = await fetch("https://sparkling-shape-65b4.niklasjuulrasmussen7.workers.dev/api/home");
-  const data = await res.json();
 
-  document.getElementById("title").innerText = data.title;
-  document.getElementById("desc").innerText = data.description;
-}
-
-loadHome();
